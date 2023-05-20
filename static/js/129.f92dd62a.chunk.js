19dcf76f71362a1fb4a8cd24fbb1d8109d06b459
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-20 08:01:08 +0000

    Deploying to gh-pages from @ EgorKrav/goit-react-hw-08-phonebook@dc4d8a163af3c9563dd92c0db61934d9fb500ac4 🚀

---
+++ static/js/129.f92dd62a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[129],{2129:function(e,t,a){a.r(t);var n=a(1267),r=a(3329);t.default=function(){return(0,r.jsx)("h2",{className:n.Z.home__title,children:"The page is not found"})}}}]);
+//# sourceMappingURL=129.f92dd62a.chunk.js.map
