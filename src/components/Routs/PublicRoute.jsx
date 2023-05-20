9156da5d67e79f5711983214c4be22077ee426d1
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-20 11:29:35 +0300

    task10

--- src/components/Routs/PublicRoute.jsx
+++ src/components/Routs/PublicRoute.jsx
@@ -1,10 +1,15 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import authSelectors from '../../redux/auth/auth-selectors';
+import PropTypes from 'prop-types';
 
 const PublicRoute = ({ children }) => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return isLoggedIn ? <Navigate to="/contacts" replace /> : children;
+};
+
+PublicRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default PublicRoute;
